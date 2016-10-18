a656de30ca3a540f7c051f441f4dd33338b8622c
Bogdan Buduroiu <bbuduroiu@gmail.com>
2016-10-18 14:42:43 +0100

    Question 3 complete

--- group-by-key.scm
+++ group-by-key.scm
@@ -1,10 +1,9 @@
+(load "assoc-all")
+(load "remove-alist-all")
+
 (define group-by-key
 	(lambda (l)
 		(if (null? l) '()
-			(begin
-				(cons 
-					(foldl (lambda (key values)
-						(cons key (cdr values)))
-						(car (car l))
-						(assoc-all (car (car l))))
-					(remove-alist-all (car (car l)) l))))))	
+			(cons 
+				(append (list (car (car l))) (assoc-all (car (car l)) l))
+				(group-by-key (remove-alist-all (car (car l)) l))))))
